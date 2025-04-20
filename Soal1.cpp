011c1121580f315ad750a16de31eca45621d0c24
syifa25 <umrimarsyifa2@gmail.com>
2025-04-20 15:05:01 +0700

    pengecekan

--- Soal1.cpp
+++ Soal1.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main (){
-  cout << "MAri kerjaka UTS";
+  string nomor;
+  cout << "Masukkan nomor kartu digital :";
+  cin >> nomor;
+
+  int panjang = nomor.length();
+  if ((panjang == 14) && (nomor[0] = '6') && (nomor[1] = '5')){
+    cout << "Tipe kartu : Nusantara"<< endl;
+  }
+  else if ((panjang == 16) && (nomor[0] = '7') && (nomor[1] = '7') || (nomor[1] = '8')){
+    cout << "Tipe kartu : Garuda"<< endl;
+  }
+  else if ((panjang == 15) && (nomor[0] = '9') && (nomor[1] = '1')){
+    cout << "Tipe kartu : Merdeka"<< endl;
+  } else {
+    cout << "Nomor kartu tidak sesuai dengan nomor kartu manapun";
+  }
+  
+
+
+
 }
