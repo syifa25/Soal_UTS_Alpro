760bcdec65cdc6e5f9c454a50f5e78797d5a3c0f
syifa25 <umrimarsyifa2@gmail.com>
2025-04-21 01:54:11 +0700

    tambahan

--- Soal1.cpp
+++ Soal1.cpp
@@ -3,23 +3,48 @@ using namespace std;
 
 int main (){
   string nomor;
+  bool valid = false;
+  int panjang;
+  int digit , posisiDariKanan;
+  int total = 0;
   cout << "Masukkan nomor kartu digital :";
   cin >> nomor;
 
-  int panjang = nomor.length();
-  if ((panjang == 14) && (nomor[0] = '6') && (nomor[1] = '5')){
+  panjang = nomor.length();
+  if ((panjang == 14) && (nomor[0] == '6') && (nomor[1] == '5')){
     cout << "Tipe kartu : Nusantara"<< endl;
+    valid = true;
   }
-  else if ((panjang == 16) && (nomor[0] = '7') && (nomor[1] = '7') || (nomor[1] = '8')){
+  else if ((panjang == 16) && (nomor[0] == '7') && (nomor[1] == '7') || (nomor[1] == '8')){
     cout << "Tipe kartu : Garuda"<< endl;
+    valid = true;
   }
-  else if ((panjang == 15) && (nomor[0] = '9') && (nomor[1] = '1')){
+  else if ((panjang == 15) && (nomor[0] == '9') && (nomor[1] == '1')){
     cout << "Tipe kartu : Merdeka"<< endl;
+    valid = true;
   } else {
     cout << "Nomor kartu tidak sesuai dengan nomor kartu manapun";
   }
-  
 
+  if (valid){
+    for (int i = panjang - 1; i >= 0; i--){
+      digit = nomor[i] - '0'; //mengubah char jadi angka
+      posisiDariKanan = panjang - i;
+
+      if (posisiDariKanan % 2 == 0){
+        digit *= 2;
+        if (digit > 9){
+          digit = (digit/10) + (digit%10);
+        }
+      }
+      total += digit;
+    }    
+    if (total % 10 == 0){
+      cout << "Nomor Kartu VALID";
+    } else {
+      cout << "Nomor Kartu TIDAK VALID";
+    }
+  }
 
 
 }
